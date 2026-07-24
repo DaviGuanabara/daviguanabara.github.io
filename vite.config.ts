177d6c1b8706ccbba093b266df5909bf02cac5f1
DaviGuanabara <davi_guanabara@live.com>
2026-07-24 10:10:42 -0300

    fixing

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,23 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   },
-  base: "/davi-guanabara.github.io/"
+  base: "/davi-guanabara.github.io/",
+  build: {
+    rollupOptions: {
+      input: {
+        app: fileURLToPath(new URL("./app.html", import.meta.url))
+      },
+      output: {
+        entryFileNames: "assets/index.js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith(".css")) {
+            return "assets/index.css";
+          }
+
+          return "assets/[name][extname]";
+        }
+      }
+    }
+  }
 });
